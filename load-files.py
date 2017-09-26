42ae1ca5cc7cbb8df6001e95dadbc6d640963ef1
James Chapman <james@paymenteconomics.com>
2017-09-26 11:18:17 -0400

    Cleaned up the Python code a bit

--- load-files.py
+++ load-files.py
@@ -18,10 +18,12 @@ save_file = "C:/Users/chja/Desktop/new_temp_ts_2017Q3.xlsx"
 def read_dsa_sheet(file_name,quarter):
     # load the data into memory
     # Now remove the non-conforming entries
-    z = pd.read_excel(file_name, sheetname="questionnaire", skiprows=7, header=None)
+    z = pd.read_excel(file_name, sheetname="questionnaire",
+                      skiprows=7, header=None)
     visits = z.loc[11:15,1:13]
     # This tests if the visit is null
-    visits.columns = ["visit_number","visit_start","visit_end","work_days","working_transit","per_diem","total_honorarium",
+    visits.columns = ["visit_number","visit_start","visit_end","work_days",
+                      "working_transit","per_diem","total_honorarium",
                       "transport","hotel","meal","hospitality","other","total"]
     x = visits[["visit_start", "visit_end"]].isnull().all(axis=1)
     visits = visits.loc[~x]
@@ -33,15 +35,21 @@ def read_dsa_sheet(file_name,quarter):
 def read_conference_sheet(file_name,quarter):
     # load the data into memory
     # Now remove the non-conforming entries
-    z = pd.read_excel(file_name,sheetname="questionnaire",skiprows=7,header=None)
-    conference = z.loc[9:14, [1, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]]
+    z = pd.read_excel(file_name,sheetname="questionnaire",
+                      skiprows=7,header=None)
+    conference = z.loc[9:14, [1, 4, 5, 6, 7, 
+                              8, 9, 10, 11,
+                              12, 13, 14, 15, 
+                              16, 17]]
     # This tests if the conference is null
     x = pd.isnull(conference[[1, 4, 5, 6, 7]]).all(axis=1)
-    # This tests if it accidentalliy added other lines
+    # This tests if it accidentalliy added other lines below the trips
     y = conference[1].str.contains("Explain") | conference[1].str.contains("Rational")
     conference = conference.loc[~(x|y)]
-    conference.columns = ["Conf_name", "start_date", "end_date", "location", "conf_link", "accepted", "departure_date",
-                          "return_date", "days", "transport", "hotel", "meals", "registration", "other", "total"]
+    conference.columns = ["Conf_name", "start_date", "end_date", "location",
+                          "conf_link", "accepted", "departure_date",
+                          "return_date", "days", "transport", "hotel",
+                          "meals", "registration", "other", "total"]
     conference['authors'] = z.loc[0, 4]
     last_name = z.loc[1,4].strip().split(" ")[1]
     conference['applicant'] = z.loc[1, 4]
@@ -75,26 +83,27 @@ dsa_data = pd.concat(dsa_data)
 
 # Now reorder the columns and rename them to a friendly version
 # trips
-app_data_order = ['dept', 'authors','paper_title', 'Conf_name','conf_link', 'location', 'departure_date',
-                  'start_date', 'end_date',  'return_date', 'days',
-                  'accepted', 'total', 'transport', 'hotel', 'meals',
-                  'registration', 'other', 'applicant',
-                  'applicant_email', 'submission_date',  'manager',
-                   'paper_link', 'times_approves',
+app_data_order = ['dept', 'authors','paper_title', 'Conf_name',
+                  'conf_link', 'location', 'departure_date', 'start_date', 
+                  'end_date',  'return_date', 'days', 'accepted', 
+                  'total', 'transport', 'hotel', 'meals',
+                  'registration', 'other', 'applicant', 'applicant_email', 
+                  'submission_date', 'manager', 'paper_link', 'times_approves',
                   'recent_quarter']
-header_names = ['Dept', 'Authors','Paper', 'Conference', 'Conference Link','Location', 'Departure',
-                  'Start', 'End',  'Return',  'Days',
-                  'Accepted', 'Total', 'Transport', 'Hotel', 'Meals',
-                  'Registration', 'Other', 'Applicant',
-                  'applicant_email', 'submission_date',  'manager',
-                   'paper_link', 'times_approves',
+header_names = ['Dept', 'Authors','Paper', 'Conference', 
+                'Conference Link','Location', 'Departure','Start', 
+                'End',  'Return',  'Days', 'Accepted', 
+                'Total', 'Transport', 'Hotel', 'Meals',
+                  'Registration', 'Other', 'Applicant', 'applicant_email',
+                  'submission_date', 'manager', 'paper_link', 'times_approves',
                   'recent_quarter']
 
 app_data = app_data[app_data_order]
 # dsas
-dsa_data_order = ['DSA_name','applicant', 'department','total','visit_number', 'visit_start', 'visit_end', 'work_days',
-       'working_transit', 'per_diem', 'total_honorarium', 'transport', 'hotel',
-       'meal', 'hospitality', 'other']
+dsa_data_order = ['DSA_name','applicant', 'department','total',
+                  'visit_number', 'visit_start', 'visit_end', 'work_days',
+                'working_transit', 'per_diem', 'total_honorarium', 'transport',
+                'hotel', 'meal', 'hospitality', 'other']
 dsa_data = dsa_data[dsa_data_order]
 
 # Now start an XlsxWriter instance
